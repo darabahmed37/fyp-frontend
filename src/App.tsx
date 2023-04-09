438086eb78a85d1950688de3edcd1ac3c3c889d2
Darab Ahmed <darab.ahmed@devsinc.com>
2023-04-09 14:19:23 +0500

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {FC} from "react";
+import {ThemeProvider} from "@mui/material";
+import theme from "./theme";
+import Authentication from "./pages/Authentication";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Authentication/>
+        </ThemeProvider>
+    );
+};
 
 export default App;
