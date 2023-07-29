eee871b95a1cfb4d44cf97976901ff940387727d
Darab Ahmed <darabahmed37@gmail.com>
2023-07-29 09:02:02 -0500

    Added dashboard

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from "react";
 import { Box, Tab, Tabs } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { logOut } from "utils/user";
 
 const Home: FC = () => {
@@ -9,14 +9,29 @@ const Home: FC = () => {
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
-
+  const navigate = useNavigate();
   return (
     <>
       <Box sx={{ width: "100%", bgcolor: "#f5f5f5", position: "relative" }}>
         <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Home" href={"/"} />
-          <Tab label="About" />
-          <Tab label="Settings" href={"/settings"} />
+          <Tab
+            label="Home"
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+          <Tab
+            label="About"
+            onClick={() => {
+              navigate("/about");
+            }}
+          />
+          <Tab
+            label="Settings"
+            onClick={() => {
+              navigate("/settings");
+            }}
+          />
           <Tab
             label={"Logout"}
             sx={{ position: "absolute", right: 0 }}
@@ -26,7 +41,14 @@ const Home: FC = () => {
           />
         </Tabs>
       </Box>
-      <Outlet />
+
+      <Box sx={{
+          maxWidth:"1280px",
+          margin:"auto",
+          marginTop:"1em"
+      }}>
+        <Outlet />
+      </Box>
     </>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { Call, More } from "@mui/icons-material";
 
 const Dashboard = () => {
-  return <div>
-jkhbgfcxdfcgvbhjlkm;,.'
-  </div>;
+  return (
+    <Box sx={{background:"#7c7dfa"}}>
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <IconButton>
+          <Call />
+        </IconButton>
+        <Box sx={{
+            background:"#e2bd3b",
+            borderRadius:"50%",
+            width:"20em",
+            padding:"1em 0"
+        }}>
+
+
+          <Typography textAlign={"center"}>Refer a friend</Typography>
+        </Box>
+
+        <IconButton>
+          <More />
+        </IconButton>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Dashboard;
