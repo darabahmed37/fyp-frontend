727bfaca370e3aee62b09bbeba811a2af3518cf9
Darab Ahmed <darabahmed37@gmail.com>
2023-07-30 07:45:58 -0500

    managed to get services from backend

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Card,
@@ -9,15 +9,7 @@ import {
   Typography,
 } from "@mui/material";
 import { Call, More } from "@mui/icons-material";
-import airConditiong from "assets/icons/air-conditioning_1060321.png";
-import towingService from "assets/icons/breakdown_3005593.png";
-import inspection from "assets/icons/diagnostic_1786559.png";
-import generator from "assets/icons/electric-generator_6232580.png";
-import key from "assets/icons/key_807241.png";
-import maintinace from "assets/icons/maintenance_1743614.png";
-import disInfection from "assets/icons/no-virus_6004151.png";
-import emergency from "assets/icons/siren_2785693.png";
-import homeService from "assets/icons/tools_3326146.png";
+import { getRequest } from "utils/axios";
 
 export interface DashboardOptions {
   image: string;
@@ -26,64 +18,15 @@ export interface DashboardOptions {
   to?: string;
 }
 
-const items: DashboardOptions[] = [
-  {
-    image: airConditiong,
-    title: "Fix Air Conditioning",
-    description:
-      "Welcome to our Car Air Conditioning Repair Application Dashboard! With user-friendly navigation and real-time updates, our dashboard provides a comprehensive overview of your vehicle's cooling system. Access diagnostics, service history, and find nearby certified technicians. Monitor refrigerant levels, track maintenance schedules, and receive timely alerts for optimal performance. Enjoy hassle-free driving with our intuitive dashboard!",
-  },
-  {
-    image: towingService,
-    title: "Car towing Service",
-    description:
-      "Introducing our car repair app's reliable car towing service! We understand the frustration of unexpected breakdowns, and that's why we offer swift, 24/7 assistance. Our skilled team ensures your vehicle is safely towed to the nearest trusted repair center. Count on us for prompt, hassle-free towing when you need it most!",
-  },
-  {
-    image: inspection,
-    title: "Inspection of Car",
-    description:
-      "Our car inspection before sale app offers a reliable and efficient car towing service to ensure your vehicle's smooth evaluation. With a network of trusted towing partners, we ensure safe transportation of your car to the inspection center. Rest easy, knowing your potential buyers will receive a thorough assessment of your car's condition.",
-  },
-  {
-    image: generator,
-    title: "Generator Service",
-    description:
-      "Introducing our reliable car towing service, an essential feature of our innovative car generator service app. When faced with unexpected breakdowns or battery failures, we've got you covered. Our team of skilled professionals is just a tap away, ensuring prompt and efficient towing to the nearest garage, giving you peace of mind on the road. Trust us to get you back on track swiftly. Download the app now and experience hassle-free assistance whenever you need it. Safety and convenience guaranteed.",
-  },
-  {
-    image: key,
-    title: "Duplicate Key",
-    description:
-      "Our car duplicate key service app offers a seamless solution for those unfortunate moments when you find yourself locked out of your vehicle. In conjunction with our trusted partners, we provide a reliable car towing service that ensures your vehicle is safely transported to your preferred location. Count on us for swift assistance and peace of mind.",
-  },
-  {
-    image: maintinace,
-    description:
-      "Our car maintenance key service app offers reliable and efficient car towing solutions to keep drivers stress-free on the road. With just a few taps, users can request prompt assistance from trusted towing professionals. Whether it's a breakdown or accident, our app ensures a seamless towing experience, providing peace of mind to our valued customers.",
-    title: "Car Maintaince",
-  },
-  {
-    image: disInfection,
-    title: "Disinfection Car",
-    description:
-      "Introducing our innovative car disinfection service app! We prioritize your safety with a 360-degree approach to sanitizing your vehicle. Our expert team employs advanced techniques and hospital-grade disinfectants to eliminate harmful germs, viruses, and bacteria. Enjoy a clean and germ-free driving experience with our convenient and reliable app. Protect your loved ones today!",
-  },
-  {
-    image: emergency,
-    title: "Emergency Service",
-    description:
-      "Introducing our cutting-edge Emergency Car Repair Service App – your ultimate companion in roadside crises. No more panic when your car breaks down unexpectedly. With just a tap, expert mechanics will swiftly reach your location, equipped with top-notch tools to fix any issue. Reliable, fast, and efficient, we'll get you back on the road in no time! Download now and drive with peace of mind.",
-  },
-  {
-    image: homeService,
-    title: "Home Service",
-    description:
-      "Our home service repair app offers efficient car repair solutions, ensuring your vehicle stays roadworthy. From routine maintenance to complex repairs, our skilled technicians are equipped to handle all car-related issues. Experience convenience at your doorstep, timely assistance, and transparent pricing, making car maintenance hassle-free and reliable for all users.",
-  },
-];
-
 const Dashboard = () => {
+  const [items, setItems] = useState<DashboardOptions[]>([]);
+
+  useEffect(() => {
+    getRequest("/services").then((response) => {
+      setItems(response.data);
+    });
+  }, []);
+
   return (
     <Box>
       <Box
@@ -126,8 +69,9 @@ const Dashboard = () => {
           paddingTop: "2em",
         }}
       >
-        {items.map((it) => (
+        {items.map((it, index) => (
           <Card
+            key={index}
             sx={{
               maxWidth: "300px",
               padding: "2em 1.4em",
@@ -141,7 +85,7 @@ const Dashboard = () => {
           >
             <CardMedia
               component={"img"}
-              image={it.image}
+              image={`${process.env.REACT_APP_BASE_BACKEND_URL}/services/${it.image}`}
               sx={{
                 objectFit: "contain",
                 maxHeight: "150px",
