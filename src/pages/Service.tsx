86908e7d167d2d31f80f9724b3d5208efc137ff9
Hafiz Ahmed <darabahmed37@gmail.com>
2023-11-16 08:55:15 -0600

    add invalid creds

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -1,102 +1,107 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { Box, Card, CardContent, Typography } from "@mui/material";
-import { getRequest } from "../utils/axios";
+import React, {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import {Box, Card, CardContent, Typography} from "@mui/material";
+import {getRequest} from "../utils/axios";
 import RatingDisplay from "../components/rating";
 import {
-  calculateDistance,
-  getCurrentLocation,
-  Location,
+    calculateDistance,
+    getCurrentLocation,
+    Location,
 } from "../utils/location";
+import {toast} from "react-toastify";
 
 const Service = () => {
-  const styles = {
-    card: {
-      width: "300px",
-      border: "2px solid #e0e0e0",
-      borderRadius: "10px",
-      boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-      transition: "transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out",
-      "&:hover": {
-        transform: "scale(1.03)",
-        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-        cursor: "pointer",
-      },
-    },
-    header: {
-      backgroundColor: "#3f51b5",
-      color: "white",
-      padding: "10px",
-      borderTopLeftRadius: "10px",
-      borderTopRightRadius: "10px",
-    },
-    content: {
-      padding: "16px",
-    },
-  };
-  const navigate = useNavigate();
-  const params = useParams();
-  const [location, setLocation] = useState<Location>();
-  const [mechanics, setMechanics] = useState<
-    {
-      user: any;
-      rating: any[];
-    }[]
-  >([]);
+    const styles = {
+        card: {
+            width: "300px",
+            border: "2px solid #e0e0e0",
+            borderRadius: "10px",
+            boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            transition: "transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out",
+            "&:hover": {
+                transform: "scale(1.03)",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+                cursor: "pointer",
+            },
+        },
+        header: {
+            backgroundColor: "#3f51b5",
+            color: "white",
+            padding: "10px",
+            borderTopLeftRadius: "10px",
+            borderTopRightRadius: "10px",
+        },
+        content: {
+            padding: "16px",
+        },
+    };
+    const navigate = useNavigate();
+    const params = useParams();
+    const [location, setLocation] = useState<Location>();
+    const [mechanics, setMechanics] = useState<
+        {
+            user: any;
+            rating: any[];
+        }[]
+    >([]);
 
-  async function setLocationState() {
-    setLocation(await getCurrentLocation());
-  }
+    async function setLocationState() {
+        try {
+            setLocation(await getCurrentLocation());
+        } catch (e) {
+            toast.error("Location Access is Disabled Can't function properly")
+        }
+    }
 
-  useEffect(() => {
-    getRequest(`/services/mechanic/${params.id}`).then((response) => {
-      setMechanics(response.data);
-      setLocationState();
-    });
-  }, [params]);
+    useEffect(() => {
+        getRequest(`/services/mechanic/${params.id}`).then((response) => {
+            setMechanics(response.data);
+            setLocationState();
+        });
+    }, [params]);
 
-  return (
-    <Box>
-      <Box>
-        <Typography variant={"h5"}>The available mechanics are</Typography>
-        <div
-          style={{
-            display: "flex",
-            gap: "10px",
-            flexWrap: "wrap",
-          }}
-        >
-          {mechanics.map((m, index) => (
-            <Card
-              key={index}
-              variant="outlined"
-              sx={styles.card}
-              onClick={() => {
-                navigate("/profile/" + m.user.id);
-              }}
-            >
-              <div style={styles.header}>
-                <Typography variant="h6">{m.user.name}</Typography>
-              </div>
-              <CardContent sx={styles.content}>
-                <Typography>Phone Number: {m.user.phoneNumber}</Typography>
-                <Typography>
-                  Distance:
-                  {location &&
-                    calculateDistance(location, {
-                      latitude: m.user.latitude,
-                      longitude: m.user.longitude,
-                    })}{" "}
-                  KM
-                </Typography>
-                <RatingDisplay m={m.rating} />
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box>
+            <Box>
+                <Typography variant={"h5"}>The available mechanics are</Typography>
+                <div
+                    style={{
+                        display: "flex",
+                        gap: "10px",
+                        flexWrap: "wrap",
+                    }}
+                >
+                    {mechanics.map((m, index) => (
+                        <Card
+                            key={index}
+                            variant="outlined"
+                            sx={styles.card}
+                            onClick={() => {
+                                navigate("/profile/" + m.user.id);
+                            }}
+                        >
+                            <div style={styles.header}>
+                                <Typography variant="h6">{m.user.name}</Typography>
+                            </div>
+                            <CardContent sx={styles.content}>
+                                <Typography>Phone Number: {m.user.phoneNumber}</Typography>
+                                <Typography>
+                                    Distance:
+                                    {location &&
+                                        calculateDistance(location, {
+                                            latitude: m.user.latitude,
+                                            longitude: m.user.longitude,
+                                        })}{" "}
+                                    KM
+                                </Typography>
+                                <RatingDisplay m={m.rating}/>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+            </Box>
+        </Box>
+    );
 };
 
 export default Service;
--- src/pages/Settings/Mechanic.tsx
+++ src/pages/Settings/Mechanic.tsx
@@ -1,96 +1,98 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import {
-  Box,
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  Typography,
+    Box,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    Typography,
 } from "@mui/material";
-import { useAppStore } from "../../store";
-import { IServices } from "../../utils/types";
+import {useAppStore} from "../../store";
+import {IServices} from "../../utils/types";
 import Button from "@mui/material/Button";
 
 import "./style.css";
-import { getRequest, postRequest } from "../../utils/axios";
-import TextArea from "../../components/text-area"; // Import the CSS file
+import {getRequest, postRequest} from "../../utils/axios";
+import TextArea from "../../components/text-area";
+import {toast} from "react-toastify"; // Import the CSS file
 
 const Mechanic = () => {
-  const services = useAppStore.use.services();
-  const [selectedServices, setSelectedServices] = useState<IServices[]>([]);
+    const services = useAppStore.use.services();
+    const [selectedServices, setSelectedServices] = useState<IServices[]>([]);
 
-  const handleServiceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedService = event.target.value;
+    const handleServiceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedService = event.target.value;
 
-    setSelectedServices((prevSelectedServices) => {
-      if (
-        prevSelectedServices.some(
-          (service) => service.title === selectedService,
-        )
-      ) {
-        return prevSelectedServices.filter(
-          (service) => service.title !== selectedService,
-        );
-      } else {
-        const serviceToAdd = services.find(
-          (service) => service.title === selectedService,
-        );
-        if (serviceToAdd) {
-          return [...prevSelectedServices, serviceToAdd];
-        }
-      }
-      return prevSelectedServices;
-    });
-  };
-  useEffect(() => {
-    getRequest("/mechanic/services").then((response) => {
-      setSelectedServices(response.data);
-    });
-  }, []);
+        setSelectedServices((prevSelectedServices) => {
+            if (
+                prevSelectedServices.some(
+                    (service) => service.title === selectedService,
+                )
+            ) {
+                return prevSelectedServices.filter(
+                    (service) => service.title !== selectedService,
+                );
+            } else {
+                const serviceToAdd = services.find(
+                    (service) => service.title === selectedService,
+                );
+                if (serviceToAdd) {
+                    return [...prevSelectedServices, serviceToAdd];
+                }
+            }
+            return prevSelectedServices;
+        });
+    };
+    useEffect(() => {
+        getRequest("/mechanic/services").then((response) => {
+            setSelectedServices(response.data);
+            toast.success("Fetched")
+        });
+    }, []);
 
-  function saveServices() {
-    postRequest("/mechanic/save-services", selectedServices).then(() => {
-      alert("services updated");
-    });
-  }
+    function saveServices() {
+        postRequest("/mechanic/save-services", selectedServices).then(() => {
+            toast.success("Added")
+        });
+    }
 
-  return (
-    <Box className="form-container">
-      <Typography variant="h3" className={"form-title"}>
-        Add More Services
-      </Typography>
-      <FormControl>
-        <FormGroup>
-          {services.map((service, index) => (
-            <FormControlLabel
-              key={index}
-              control={
-                <Checkbox
-                  className="form-checkbox"
-                  checked={selectedServices.some(
-                    (s) => s.title === service.title,
-                  )}
-                  onChange={handleServiceChange}
-                  value={service.title}
-                />
-              }
-              label={service.title}
-            />
-          ))}
-        </FormGroup>
-        <Button
-          className="save-button"
-          variant="contained"
-          onClick={saveServices}
-          color={"success"}
-        >
-          Save
-        </Button>
-      </FormControl>
+    return (
+        <Box className="form-container">
+            <Typography variant="h3" className={"form-title"}>
+                Add More Services
+            </Typography>
+            <FormControl>
+                <FormGroup>
+                    {services.map((service, index) => (
+                        <FormControlLabel
+                            key={index}
+                            control={
+                                <Checkbox
+                                    className="form-checkbox"
+                                    checked={selectedServices.some(
+                                        (s) => s.title === service.title,
+                                    )}
+                                    onChange={handleServiceChange}
+                                    value={service.title}
+                                />
+                            }
+                            label={service.title}
+                        />
+                    ))}
+                </FormGroup>
+                <Button
+                    className="save-button"
+                    variant="contained"
+                    onClick={saveServices}
+                    color={"success"}
+                >
+                    Save
+                </Button>
+            </FormControl>
 
-      <TextArea />
-    </Box>
-  );
+            <TextArea/>
+        </Box>
+    );
 };
 
 export default Mechanic;
